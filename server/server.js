fd0708e33ef5f5bcf23176fd035b7e274ed4ba7c
Cameron Nye <csnye86@gmail.com>
2018-07-24 14:07:45 -0600

    first commit. bare server running

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+    , express = require('express')
+    , session = require('express-session')
+    , massive = require('massive')
+
+const {
+  SERVER_PORT
+} = process.env
+
+
+const app = express()
+app.use(bodyParser.json())
+
+
+
+app.listen(SERVER_PORT, console.log(`🐦  The birds are chirping on port ${SERVER_PORT} 🐦`))
