8322df90e7cad26127f96f0829813ba4a0a0473b
Cameron Nye <csnye86@gmail.com>
2018-08-03 16:20:26 -0600

    Nodemailer backend built. Home and Nav responsive

--- server/server.js
+++ server/server.js
@@ -3,14 +3,46 @@ const bodyParser = require('body-parser')
     , express = require('express')
     , session = require('express-session')
     , massive = require('massive')
+    , nodemailer = require('nodemailer')
 
 const {
-  SERVER_PORT
+  SERVER_PORT,
+  EMAIL,
+  EMAIL_PASSWORD
 } = process.env
 
 
 const app = express()
 app.use(bodyParser.json())
+
+// NODEMAILER
+let transport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: EMAIL,
+    pass: EMAIL_PASSWORD
+  }
+})
+
+app.post('/email', (req, res, next) => {
+  const {name, location, email, subject, message} = req.body
+  let mail = {
+    from: EMAIL,
+    to: EMAIL,
+    html: `Sender: ${name} from ${location} <br/> Sender's Email: ${email} <br/> Subject: ${subject} <br/> Message: ${message}` 
+  }
+  transporter.sendMail(mail, (error, response) => {
+    if(error){
+      console.log('Error sending email')
+      console.log(error)
+    }
+    else {
+      console.log('Email sent successfully!')
+    }
+    transporter.close()
+  })
+  res.sendStatus(201)
+})
 
 
 
