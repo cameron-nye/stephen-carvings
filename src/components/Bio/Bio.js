74cdbaf7fa54c0b274eb2246c00a4630bad6e611
Cameron Nye <csnye86@gmail.com>
2018-08-30 15:01:29 -0600

    Contact styled. Edited media query ranges for Home & Nav

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -4,7 +4,29 @@ export default class Bio extends Component {
   render() {
     return (
       <div>
-        Bio
+        From as early as 4 years I remember having a fascination with God’s 
+        creations and a love of the great outdoors. My first passion was 
+        geology and rock collecting. Later my interests turned to weather, 
+        wildlife, art, and over the past 40 years, birding. From an early age 
+        I also enjoyed creating and making things with my hands which 
+        eventually led me to a career in building high-end furniture and 
+        architectural woodwork. 
+        <br/>
+        Though I did study sculpture while earning a
+        degree in Range Management at BYU, as a woodcarver I am primarily 
+        self-taught. My art is driven by the feelings, impressions and 
+        memories of a life spent out of doors on planet earth in wondrous awe 
+        of our avian companions. 
+        <br/>
+        I have received numerous awards over the 
+        years including Best of Show, the Audubon Society Choice Award, the 
+        People’s Choice Award, the Wheeler Historic Farm Choice Award, and 
+        numerous Best of Division Awards. I also have a piece in the 
+        collection of Richard and Heidi Shelley. 
+        <br/>
+        I’m grateful to my Heavenly Father who has given me a talent to express the special moments and 
+        feelings I experience in nature through carving. I am also thankful 
+        for the joy and relaxation it brings to my life.
       </div>
     )
   }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,93 +1,122 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 import TextField from "@material-ui/core/TextField";
 //Style
-import './Contact.css'
+import "./Contact.css";
 
 export default class Contact extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      name: '',
-      email: '',
-      location: '',
-      subject: '',
-      message: '',
-    }
+      name: "",
+      email: "",
+      location: "",
+      subject: "",
+      message: ""
+    };
   }
 
-  inputHandler(e, property){
+  inputHandler(e, property) {
     this.setState({
       [property]: e.target.value
-    })
+    });
   }
 
-  handleSubmit(){
-    let {name, email, location, subject, message} = this.state
-    axios.post('/email', {name, email, location, subject, message})
+  handleSubmit() {
+    let { name, email, location, subject, message } = this.state;
+    axios
+      .post("/email", { name, email, location, subject, message })
       .then(() => {
         this.setState({
-          name: '',
-          email: '',
-          location: '',
-          subject: '',
-          message: '',
-        })
+          name: "",
+          email: "",
+          location: "",
+          subject: "",
+          message: ""
+        });
       })
-      .catch((error) => {
-        console.log(error)
-      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-      <div className='contact'>
-        <TextField 
-          style= {{width: 200}}
-          id='name'
-          label='What is your name?'
-          value={this.state.name}
-          onChange={(e) => {this.inputHandler(e, 'name')}}
-          margin='normal'
-        />
-        <TextField 
-          style= {{width: 200}}
-          id='email'
-          label='What is your email?'
-          value={this.state.email}
-          onChange={(e) => {this.inputHandler(e, 'email')}}
-          margin='normal'
-        />
-        <TextField 
-          style= {{width: 200}}
-          id='location'
-          label='Where are you from?'
-          value={this.state.location}
-          onChange={(e) => {this.inputHandler(e, 'location')}}
-          margin='normal'
-        />
-        <TextField 
-          style= {{width: 200}}
-          id='subject'
-          label='Subject'
-          value={this.state.subject}
-          onChange={(e) => {this.inputHandler(e, 'subject')}}
-          margin='normal'
-        />
-        <TextField
-          multiline
-          rowsMax="100"
-          fullWidth
-          id="message"
-          label="How can I help you?"
-          value={this.state.message}
-          onChange={e => {this.inputHandler(e, "message");}}
-          margin="normal"
-        />
-        <div className="sendButton" onClick={() => {this.handleSubmit()}}>
-          Submit
+      <div className="contact">
+        <p>Contact Me</p>
+        <div className="contactWrapper">
+          <div className="name">
+            <TextField
+              style={{ width: 250 }}
+              id="name"
+              label="What is your name?"
+              value={this.state.name}
+              onChange={e => {
+                this.inputHandler(e, "name");
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className="email">
+            <TextField
+              style={{ width: 250 }}
+              id="email"
+              label="What is your email?"
+              value={this.state.email}
+              onChange={e => {
+                this.inputHandler(e, "email");
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className="location">
+            <TextField
+              style={{ width: 250 }}
+              id="location"
+              label="Where are you from?"
+              value={this.state.location}
+              onChange={e => {
+                this.inputHandler(e, "location");
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className="subject">
+            <TextField
+              style={{ width: 250 }}
+              id="subject"
+              label="Subject"
+              value={this.state.subject}
+              onChange={e => {
+                this.inputHandler(e, "subject");
+              }}
+              margin="normal"
+            />
+          </div>
+          <div className="message">
+            <TextField
+              multiline
+              rowsMax="100"
+              fullWidth
+              id="message"
+              label="How can I help you?"
+              value={this.state.message}
+              onChange={e => {
+                this.inputHandler(e, "message");
+              }}
+              margin="normal"
+            />
+          </div>
+          <div
+            className="sendButton"
+            onClick={() => {
+              this.handleSubmit();
+            }}
+          >
+            Submit
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
